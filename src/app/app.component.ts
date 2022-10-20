efd03ed6a8e8b25a0c2b02937287f226dce64e8a
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-10-19 22:40:47 -0400

    NgOnInit app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,7 @@ export class AppComponent implements OnInit{
     return new Promise((resolve, reject) => {
       if(nome === 'Eduardo'){
         setTimeout(() =>{
-          resolve('Seja bem vindo' + nome);
+          resolve('Seja bem vindo!' + nome);
         }, 1000);
       }
       else{
